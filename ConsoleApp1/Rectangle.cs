c9c978ff39d4a9525d761b8e3a3e2d97e510401a
Jenish Nakrani <Jnakrani5508@conestogac.on.ca>
2022-10-04 21:13:12 -0400

    completed Rectangle class

---
+++ ConsoleApp1/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Rectangle
+    {
+        private int length;
+        private int width;
+
+        //default constructor
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+        //non-default constructor
+        public Rectangle(int rLength, int rWidth)
+        {
+            length = rLength;
+            width = rWidth;
+        }
+        public int GetCurrentLength()
+        {
+            return length;
+        }
+        public int SetNewLength(int rLength)
+        {
+            length = rLength;
+            return length;
+        }
+        public int GetCurrentWidth()
+        {
+            return width;
+        }
+        public int SetNewWidth(int rWidth)
+        {
+            width = rWidth;
+            return width;
+        }
+        public int GetPerimeter()
+        {
+            //double perimeter = 2 * (length + width);
+            return 2 * (length + width);
+        }
+        public int GetArea()
+        {
+            return length * width;
+        }
+    }
+}
